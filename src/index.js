32b4a720e25fb3a0d4c2f4a015793f713e17a19e
MountainGirl504 <natalia_calt@yahoo.com>
2017-10-10 14:34:30 -0600

    Finished mini project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {HashRouter} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from "react-redux";
 
 import store from './store';
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <App />
-  </Provider>, 
-  document.getElementById('root')
+  <HashRouter>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </HashRouter>,
+    document.getElementById('root')
 );
 registerServiceWorker();
